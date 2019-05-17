78963163ff646b6f851b67d96f195aa0ba600a55
Jon <lazarini.jonathan@gmail.com>
2019-05-17 12:10:20 +0100

    add filter functionality per tags, add more data

--- src/Article.elm
+++ src/Article.elm
@@ -6,7 +6,20 @@ tags =
 
 
 feed =
-    [ { description = "some description"
+    [ { title = "a title"
+      , description = "some description"
       , tags = [ "elm" ]
+      }
+    , { title = "another title"
+      , description = "some other description"
+      , tags = [ "vier" ]
+      }
+    , { title = "title"
+      , description = "some description"
+      , tags = [ "elm" ]
+      }
+    , { title = "title"
+      , description = "some description"
+      , tags = [ "tag2" ]
       }
     ]
--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,7 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import List exposing (map)
+import List exposing (filter, map, member)
 
 
 type alias Model =
@@ -23,7 +23,8 @@ initialModel =
 
 
 type alias Article =
-    { description : String
+    { title : String
+    , description : String
     , tags : List String
     }
 
@@ -42,16 +43,24 @@ update msg model =
         model
 
 
-viewTags tags =
+viewTags selectedTag tags =
     let
         renderedTags =
-            map renderTag tags
+            map (renderTag selectedTag) tags
     in
     div [ class "tag-list" ] renderedTags
 
 
-renderTag tagName =
-    button [ class "tag-pill tag-default" ] [ text tagName ]
+renderTag selectedTag tagName =
+    let
+        otherCssClass =
+            if tagName == selectedTag then
+                "tag-selected"
+
+            else
+                "tag-default"
+    in
+    button [ class ("tag-pill " ++ otherCssClass), onClick { description = "ClickedTag", data = tagName } ] [ text tagName ]
 
 
 renderBanner =
@@ -63,32 +72,31 @@ renderBanner =
         ]
 
 
-renderArticles articles =
-    let
-        renderedArticles =
-            map renderArticle articles
-    in
-    ul [ class "some class" ] renderedArticles
-
-
 renderArticle article =
-    li []
-        [ text article.description
-
-        -- render tags later--
+    div [ class "article-preview" ]
+        [ h1 [] [ text article.title ]
+        , p [] [ text article.description ]
+        , span [] [ text "Read more..." ]
         ]
 
 
 view model =
+    let
+        articles =
+            filter (\a -> member model.selectedTag a.tags) model.articlesFeed
+
+        articleFeed =
+            map renderArticle articles
+    in
     div [ class "home-page" ]
         [ div [] [ renderBanner ]
         , div [ class "container page" ]
             [ div [ class "row" ]
-                [ div [ class "col-md-9" ] [ renderArticles model.articlesFeed ]
+                [ div [ class "col-md-9" ] articleFeed
                 , div [ class "col-md-3" ]
                     [ div [ class "sidebar" ]
                         [ p [] [ text "Popular Tags" ]
-                        , viewTags model.tags
+                        , viewTags model.selectedTag model.tags
                         ]
                     ]
                 ]
